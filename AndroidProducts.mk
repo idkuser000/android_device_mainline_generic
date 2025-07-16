b2b4542d23137e4f1ce1d8e44ceba3a847ce9108
Yumi Yukimura <me.cafebabe@gmail.com>
2025-07-17 01:02:11 +0800

    basic_x86_64_pc: Add AOSP product makefile
    Change-Id: I7b1c17b8c9a8eb52073b207f9c68595ddd8e1033

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,7 +4,9 @@
 #
 
 PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_basic_x86_64_pc.mk \
     $(LOCAL_DIR)/lineage_basic_x86_64_pc.mk
 
 $(foreach build_type, user userdebug eng, \
+    $(eval COMMON_LUNCH_CHOICES += aosp_basic_x86_64_pc-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += lineage_basic_x86_64_pc-$(build_type)))
