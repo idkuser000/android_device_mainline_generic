858df6a2b6ef6a6033d88c6246cf7fa2cb7c60c2
Yumi Yukimura <me.cafebabe@gmail.com>
2026-06-09 20:13:15 +0800

    mainline/generic: build: Add phony target for building all the wanted images
    Change-Id: Ic5f99342a9c563ecdb6ef4793540f7bcd07ad570

---
+++ build/tasks/90-all_images.mk
@@ -0,0 +1,31 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq ($(USES_DEVICE_MAINLINE_GENERIC),true)
+
+# product and system_ext could be here if we add them later on
+LOCAL_WANTED_SYSTEM_IMAGES := \
+    system
+
+LOCAL_WANTED_SYSTEM_IMAGES_FILES := \
+    $(addsuffix .img,$(LOCAL_WANTED_SYSTEM_IMAGES))
+
+# We aren't GKI compatible so kernel and system_dlkm counts into vendor for us...
+LOCAL_WANTED_VENDOR_IMAGES := \
+    ramdisk \
+    system_dlkm \
+    vendor \
+    vendor_dlkm
+
+LOCAL_WANTED_VENDOR_IMAGES_FILES := \
+    $(addsuffix .img,$(LOCAL_WANTED_VENDOR_IMAGES)) \
+    kernel
+
+.PHONY: all_images all_system_images all_vendor_images
+all_images: $(addprefix $(PRODUCT_OUT)/,$(LOCAL_WANTED_SYSTEM_IMAGES_FILES) $(LOCAL_WANTED_VENDOR_IMAGES_FILES))
+all_system_images: $(addprefix $(PRODUCT_OUT)/,$(LOCAL_WANTED_SYSTEM_IMAGES_FILES))
+all_vendor_images: $(addprefix $(PRODUCT_OUT)/,$(LOCAL_WANTED_VENDOR_IMAGES_FILES))
+
+endif # USES_DEVICE_MAINLINE_GENERIC
