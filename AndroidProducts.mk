4fec12652d1b22d4750845864ea2f55195996b3d
cgik <idkzzz50@gmail.com>
2026-09-04 18:16:30 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,12 +5,12 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/Generic_arm64/aosp_Generic_arm64.mk \
-    $(LOCAL_DIR)/Generic_arm64/lineage_Generic_arm64.mk \
+    $(LOCAL_DIR)/Generic_arm64/custom_Generic_arm64.mk \
     $(LOCAL_DIR)/Generic_x86_64/aosp_Generic_x86_64.mk \
-    $(LOCAL_DIR)/Generic_x86_64/lineage_Generic_x86_64.mk
+    $(LOCAL_DIR)/Generic_x86_64/custom_Generic_x86_64.mk
 
 $(foreach build_type, user userdebug eng, \
     $(eval COMMON_LUNCH_CHOICES += aosp_Generic_arm64-$(build_type)) \
     $(eval COMMON_LUNCH_CHOICES += aosp_Generic_x86_64-$(build_type)) \
-    $(eval COMMON_LUNCH_CHOICES += lineage_Generic_arm64-$(build_type)) \
-    $(eval COMMON_LUNCH_CHOICES += lineage_Generic_x86_64-$(build_type)))
+    $(eval COMMON_LUNCH_CHOICES += custom_Generic_arm64-$(build_type)) \
+    $(eval COMMON_LUNCH_CHOICES += custom_Generic_x86_64-$(build_type)))
