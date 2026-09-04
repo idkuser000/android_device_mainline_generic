98dc0840202a38f2c3d85e3189cdbf8530bdf76e
cgik <idkzzz50@gmail.com>
2026-09-04 18:15:25 +0700

    Update custom_Generic_x86_64.mk

--- Generic_x86_64/custom_Generic_x86_64.mk
+++ Generic_x86_64/custom_Generic_x86_64.mk
@@ -8,7 +8,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/custom/config/common_full_tablet_wifionly.mk)
 
 # Inherit from device
 $(call inherit-product, device/mainline/generic/Generic_x86_64/device.mk)
